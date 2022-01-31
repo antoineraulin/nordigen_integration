7a36580cf21d9a671892aa5b464cefecf49d5735
Antoine Raulin <antoin.raulin@gmail.com>
2022-01-31 11:54:30 +0100

    implemented getCategoryTree

--- lib/extensions/categories.dart
+++ lib/extensions/categories.dart
@@ -1,3 +1,25 @@
 part of 'package:nordigen_integration/nordigen_integration.dart';
 
-extension NordigenCategorisationEndpoints on NordigenAccountInfoAPI {}
+extension NordigenCategorisationEndpoints on NordigenAccountInfoAPI {
+  /// Retrieve the latest category tree
+  ///
+  /// [country] : Two letter country code filter for categories
+  Future<List<CategoryData>> getCategoryTree({
+    required String country,
+  }) async {
+    assert(country.isNotEmpty);
+    assert(country.length == 2);
+
+    // Make GET request and fetch output.
+    final dynamic fetchedData = await _nordigenGetter(
+      endpointUrl: 'https://api.nordigen.com/v2/category-tree/$country',
+    );
+
+    // Convert fetched data to a list of CategoryData objects.
+    List<dynamic> rawCategories =
+        fetchedData['data']['attributes']['categories'];
+    return rawCategories
+        .map((dynamic rawCategory) => CategoryData.fromMap(rawCategory))
+        .toList();
+  }
+}
